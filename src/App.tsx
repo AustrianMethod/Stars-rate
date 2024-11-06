268b4ed3e26d1d761b1a9f6fb36d0d9a58884e03
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-11-06 12:07:49 +0300

    done

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+
+import './App.css'
+
+interface StarsProps {
+  count: number;
+}
+
+function Star() {
+  return (
+    <li>
+      <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>
+            <path d="M0 0h18v18H0z" fill="none"/>
+      </svg>
+    </li>
+  )
+}
+
+function Stars({count}: StarsProps) {
+
+
+  const isNumber = (value: number): boolean => {
+    return typeof value === 'number' && !isNaN(value);
+  }
+
+  if (!isNumber(count) || count < 1 || count > 5) {
+    return null;
+  }  
+
+  const stars = Array.from({ length: count }, (_, i) => <Star key={i} />);
+
+  return (
+    <ul 
+      className="card-body-stars u-clearfix"
+      style={{ listStyleType: 'none'}}
+    >
+      {stars}
+    </ul>
+  );
+}
+
+function App() {
+
+  return (
+    <>
+      <Stars count={4}/>
+    </>
+  )
+}
+
+export default App
